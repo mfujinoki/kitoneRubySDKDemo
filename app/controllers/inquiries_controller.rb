0d77ce5b4cf2a4a8d8e3a9273e782978a44d0cc2
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2018-01-23 11:19:37 -0800

    Delete query model

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,7 +1,4 @@
 class InquiriesController < ApplicationController
-  def show
-    @inquiry = Inquiry.find(params[:id])
-  end
 
   def new
   end
@@ -9,22 +6,26 @@ class InquiriesController < ApplicationController
   def create
     @inquiry = Inquiry.new(inquiry_params)
 
-    @inquiry.save
+    if @inquiry.save
 
-    # Use token authentication
-    api = Kintone::Api.new("devxorudc.cybozu.com", "rBoD1vaqme5y5ezC38MVWdpoUAilIUYP4Rs8OwIO")
+      # Use token authentication
+      api = Kintone::Api.new("devxorudc.cybozu.com", "rBoD1vaqme5y5ezC38MVWdpoUAilIUYP4Rs8OwIO")
 
-    # Record register(single record)
-    # Use Hash
-    app = 103
-    record = {"Customer" => {"value" => inquiry_params[:company_name]},
-              "client_name" => {"value" => inquiry_params[:client_name]},
-              "QType" => {"value" => inquiry_params[:contact_type]},
-              "Detail" => {"value" => inquiry_params[:details]}
-              }
-    api.record.register(app, record)
+      # Record register(single record)
+      # Use Hash
+      app = 103
+      record = {"Customer" => {"value" => inquiry_params[:company_name]},
+                "client_name" => {"value" => inquiry_params[:client_name]},
+                "QType" => {"value" => inquiry_params[:contact_type]},
+                "Detail" => {"value" => inquiry_params[:details]}
+                }
+      api.record.register(app, record)
+      redirect_to root_path
+    else
+      render 'new'
+    end
 
-    redirect_to root_path
+
   end
   private
     def inquiry_params
